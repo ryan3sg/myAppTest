42ba8b64d8a44ef7f8b4b8c814e3f44042905aaa
Rye Ayan <ryan3sg@users.noreply.github.com>
2018-08-21 11:19:49 +0800

    adding files

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="container" onresize="windowSize">
+    <h1>{{msg}}</h1>
+    <p>Sample</p>
+    <p>{{windowWidth}}</p>
+    <p>{{windowHeight}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComponent',
+  data () {
+    return {
+      msg: 'Whats your plan for today?',
+      windowWidth: 0,
+      windowHeight: 0
+    }
+  },
+  created: function () {
+    this.windowWidth = window.outerWidth
+    this.windowHeight = window.outerHeight
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: normal;
+  }
+</style>
