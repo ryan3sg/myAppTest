4708f288928fb5a88479f197d0405ac9b1ee093e
Rye Ayan <ryan3sg@users.noreply.github.com>
2018-08-26 12:34:56 +0800

    added vuex

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import store from '@/vuex/store'
+import getters from '@/vuex/getters'
+import actions from '@/vuex/actions'
+import mutation from '@/vuex/mutation'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+Vue.use(Vuex)
+
 new Vue({
   el: '#app',
   router,
+  store: store,
+  getters: getters,
+  actions: actions,
+  mutation: mutation,
   components: { App },
   template: '<App/>'
 })
